f11f686610929dbc673c345f513c1b0c7c76665a
LuisQN <133921397+LuisQN@users.noreply.github.com>
2023-05-18 18:59:45 -0300

    modelo
    crear modelo socio

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Socio(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField
+    activo = models.BooleanField(default= True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
