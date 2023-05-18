34ea99b4fad8663041791dd08ee6a8c4e99f0dbe
Kev <kevinserrano010@gmail.com>
2023-05-18 11:33:33 -0300

    Registre el modelo de Prestamo de Libro en el Admin
    Tarea realizada

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from biblioteca.models import PrestamoLibro
 
-# Register your models here.
+# Tarea de Kev
+class PrestamoLibroAdmin(admin.ModelAdmin):
+    model = PrestamoLibro
+    list_display = ("id", "fecha_prestamos", "fecha_devolucion", "socio", "empleado", "libro")
+    list_search = ("socio", "libro", "empleado")
+
+
+
+
+admin.site.register(PrestamoLibro, PrestamoLibroAdmin)
