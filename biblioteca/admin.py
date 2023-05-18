8a769b178321ddcee2efca78c0386facf4e6a489
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 20:21:46 -0300

    Modelo Admin Empleo
    Creación del Modelo Admin empleo

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -3,5 +3,13 @@ from biblioteca.models import Empleado
 # Register your models here.
 
 
+class EmpleadoAdmin(admin.ModelAdmin):
+    model=Empleado
+    list_display=("id","nombre","apellido","activo")
+    search_fields=("nombre",)
+    list_filter=("activo",)
+    
 
+
+admin.site.register(Empleado, EmpleadoAdmin)
 
