55d66f37796bcc55273201745fef1e779784bb06
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 18:41:06 -0300

    Cambios del modelo
    Modelo Libro
    Vista Libro
    Admin

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from biblioteca.models import Autor,Libro ##
 # Register your models here.
+
+class LibroAdmin(admin.ModelAdmin):
+    model=Libro
+    list_display=("id","titulo","descripcion","isbn","activo","autor")
+    search_fields=("titulo","autor__nombre")
+    list_filter=("autor__nombre",)
+
+
+admin.site.register(Libro, LibroAdmin) ##
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    pass
+
+class Libro(models.Model):
+    titulo=models.CharField(max_length=80) #consulta si ponemos longitud
+    descripcion=models.CharField(max_length=80) #idem
+    isbn=models.IntegerField(default=0) #idem
+    activo=models.BooleanField(default=True)
+    autor=models.ForeignKey(
+        Autor,
+        related_name="libros",
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return f"{self.titulo} - {self.autor}"
+
