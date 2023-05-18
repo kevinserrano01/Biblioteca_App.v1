354d0e2d0e4a0f70e0b5cb8b8e93c6938f627274
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 20:47:35 -0300

    update view
    se borro el la funcion prueba

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from biblioteca.models import models Empleado
 
-def funcioGus(request):
-    pass
-#prueba  las 00.33 am
+
 # Funcion de Kev
 def desactivar_Registro_Empleado(request, empleado_id):
     empleado = Empleado.objects.get(id=empleado_id)
