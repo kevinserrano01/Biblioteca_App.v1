29c1c1575e850e20b32143486e312fe68154c10d
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 19:59:58 -0300

    eliminación de codigo
    se elimino algo que no era mio

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from biblioteca.models import Autor,Libro ##
 # Register your models here.
 
-class LibroAdmin(admin.ModelAdmin):
-    model=Libro
-    list_display=("id","titulo","descripcion","isbn","autor","activo")
-    search_fields=("titulo","autor__nombre")
-    list_filter=("autor__nombre",)
 
 
-admin.site.register(Libro, LibroAdmin) ##
+
