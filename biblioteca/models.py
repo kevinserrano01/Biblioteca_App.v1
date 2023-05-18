9e1a45dee0178674824e61742b4a7f7da0af031d
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 19:41:53 -0300

    se combino al ramas

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+# Tarea de Kev
+class PrestamoLibro(models.Model):
+    fecha_prestamos = models.DateField()
+    fecha_devolucion = models.DateField()
+    socio = models.ForeignKey(
+        Socio,
+        on_delete=models.CASCADE
+    )
+    empleado: models.ForeignKey(
+        Empleado,
+        on_delete=models.CASCADE
+    )
+    libro: models.ForeignKey(
+        Libro,
+        on_delete=models.CASCADE
+    )
 
 
 
