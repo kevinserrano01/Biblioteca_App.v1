628b489dde33efac61e76b5fbaf4ea1029917958
Kev <kevinserrano010@gmail.com>
2023-05-19 16:32:13 -0300

    Creacion de modelo Autor
    Tarea realizada

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+class Autor(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    nacionalidad=models.CharField(max_length=50)
+    activo=models.BooleanField(default=True)
 
-
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido}"
 
 
 #Modelo libro byGus
