bcbf8d326cd30c3d07a243f3ec5b272aa46e2b99
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-19 00:16:44 -0300

    cambio en urls path
    Se cambio la / al comienzo del para que quede como se pedia en el proyecto

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     
     path('/empleados/desactivar/<int:id>', views.desactivar_Registro_Empleado, name='desactivar_Registro_Empleado'),
-    path('empleados_listado/', views.listado_empleados, name="listado_empleados"),
+    path('empleados/listado/', views.listado_empleados, name="listado_empleados"),
 ]
