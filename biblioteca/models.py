a7a24dbfcc9833c9b6d89c7926204a0e5abc5ae2
Kev <kevinserrano010@gmail.com>
2023-05-19 19:00:13 -0300

    Medelo empleado
    Tarea ralizada

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 #Tarea Andy
 class Empleado(models.Model):
-     nombre:models.CharField(max_length=30)
-     apellido:models.CharField(max_length=30)
-     numero_legajo:models.IntegerField
-     activo:models.BooleanField
-     def __str__(self):
+    nombre:models.CharField(max_length=30)
+    apellido:models.CharField(max_length=30)
+    numero_legajo:models.IntegerField
+    activo:models.BooleanField
+    
+    def __str__(self):
         return f'{self.nombre} - {self.apellido} - {self.numero_legajo} - {self.activo}'
 
 # Tarea de Kev
