d41a25e8ac63de7f7d6d74bfb5d36b0f1872a305
Kev <kevinserrano010@gmail.com>
2023-05-19 16:36:10 -0300

    Registro del modelo Empleado en el Admin
    Tarea realizada

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from biblioteca.models import Empleado, Socio
 # Register your models here.
 
+class EmpleadoAdmin(admin.ModelAdmin):
+    model=Empleado
+    list_display=("id","nombre","apellido","activo")
+    search_fields=("nombre",)
+    list_filter=("activo",)
 
 class EmpleadoAdmin(admin.ModelAdmin):
     model=Empleado
