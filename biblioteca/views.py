212e7e73dd76baea06f5aea1790ca4ca6d1a43da
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-18 23:07:13 -0300

    update view. empleados

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -9,3 +9,12 @@ def desactivar_Registro_Empleado(request, empleado_id):
     empleado.activo = False
     empleado.save()
     return HttpResponse(f'El empleado con el ID: {empleado_id} fue ELIMINADO!')
+
+
+def listado_empleados(request):
+    empleados=Empleado.objects.all()
+    context={
+        "empleados":empleados,
+
+    }
+    return render(request, "Empleados_lista.html", context)
