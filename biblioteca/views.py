1522bcb3643d95b0ff01b7c5fe82a3829020bf0e
LuisQN <133921397+LuisQN@users.noreply.github.com>
2023-05-19 16:24:15 -0300

    tareas sprint 1
    modelo Socio, registro Autor en admin y formulario actualizar empleado realizados

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from biblioteca.models import models Empleado
 
 # Funcion de Kev
@@ -8,3 +8,19 @@ def desactivar_Registro_Empleado(request, empleado_id):
     empleado.activo = False
     empleado.save()
     return HttpResponse(f'El empleado con el ID: {empleado_id} fue ELIMINADO!')
+
+
+# funcion de Luis
+def actualizar_empleado(request, empleado_id):
+    empleado = get_object_or_404(Empleado, pk=empleado_id)   
+    if request.method == "POST":
+        empleado.nombre = request.POST["nombre"]
+        empleado.apellido = request.POST["apellido"]
+        empleado.numero_legajo = request.POST["numero_legajo"]
+        empleado.activo = True if request.POST.get("activo") == "on" else False
+        empleado.save()
+
+    context = {
+            'empleado': empleado,
+        }
+    return render(request, "formulario_actualizar_empleado.html", context)
