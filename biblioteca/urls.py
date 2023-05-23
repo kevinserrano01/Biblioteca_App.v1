04ee3ca96854991ec38be0ff8f4582e740fe9d85
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-05-23 13:39:24 -0300

    Cambios en nav

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('empleados/listado/', views.listado_empleados, name="listado_empleados"),
     path('empleados/crear_empleado/', views.nuevo_empleado, name='nuevo_empleado'),
     path('actualizar_empleado/<int:empleado_id>/', views.actualizar_empleado, name='actualizar_empleado'),
-    path('empleados/activar/<int:empleado_id>', views.activar_Registro_Empleado, name='activar_Registro_Empleado'),
+    path('autores/desactiva/<int:autor_id>', views.desactivar_Registro_Autor, name='desactivar_Registro_Autor'),    
 ]
