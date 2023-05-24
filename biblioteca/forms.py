b8af6443700756e5b26b4ea4591308b231607843
LuisQN <133921397+LuisQN@users.noreply.github.com>
2023-05-24 17:36:54 -0300

    segundo sprint
    tareas realizadas:
    view para activar el registro de un autor
    view para desactivar el registro de un socio
    formulario para registrar nuevos autores
    formulario para registrar nuevos socios

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -4,3 +4,15 @@ class CrearNuevoEmpleado(forms.Form):
     nombre = forms.CharField(label='Nombre del empleado', max_length=200)
     apellido = forms.CharField(label='Apellido del empleado', max_length=200)
     nro_legajo = forms.CharField(label='Numero de legajo', max_length=200)
+
+
+class CrearNuevoAutor(forms.Form):
+    nombre = forms.CharField(label='Nombre del autor', max_length=50)
+    apellido = forms.CharField(label='Apellido del autor', max_length=50)
+    nacionalidad = forms.CharField(label='Numero de legajo', max_length=50)
+
+
+class CrearNuevoSocio(forms.Form):
+    nombre = forms.CharField(label='Nombre del autor', max_length=50)
+    apellido = forms.CharField(label='Apellido del autor', max_length=50)
+    fecha_nacimiento = forms.DateField(label='Fecha de nacimiento')
