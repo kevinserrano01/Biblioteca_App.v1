5284f92f3e06a2264d694f4241dce0c017af1a94
Kev <kevinserrano010@gmail.com>
2023-05-24 18:47:21 -0300

    update files

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('empleados/listado/', views.listado_empleados, name="listado_empleados"),
     path('empleados/crear_empleado/', views.nuevo_empleado, name='nuevo_empleado'),
     path('actualizar_empleado/<int:empleado_id>/', views.actualizar_empleado, name='actualizar_empleado'),
-    path('autores/desactiva/<int:autor_id>', views.desactivar_Registro_Autor, name='desactivar_Registro_Autor'),    
+    path('autores/desactiva/<int:autor_id>', views.desactivar_Registro_Autor, name='desactivar_Registro_Autor'),
 ]
