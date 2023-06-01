4f853b60df54efe996151665d72ddd2389b49c40
Naira <88989496+Naikl12@users.noreply.github.com>
2023-06-01 01:53:58 -0300

    urls de api

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('libros/', views.listado_libros, name='listado_libros')
+    path('libros/',
+         views.listado_libros, 
+         name='listado_libros'
+    ),
 ]
