6d8e27af1c00bb0adde45871154452a8b688aa20
Naira <88989496+Naikl12@users.noreply.github.com>
2023-05-31 22:34:30 -0300

    agregue formulario para actualizar un libro existente

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -1,21 +1,10 @@
 from django import forms
-from biblioteca.models import Autor, Empleado, Socio
+from biblioteca.models import Autor, Empleado, Socio, Libro
 
 class CrearNuevoEmpleado(forms.Form): # Kev
     nombre = forms.CharField(label='Nombre del empleado', max_length=200)
     apellido = forms.CharField(label='Apellido del empleado', max_length=200)
     nro_legajo = forms.CharField(label='Numero de legajo', max_length=200)
-
-class ActualizarAutor(forms.ModelForm): # Kev
-    class Meta:
-        model = Autor
-        fields = ('__all__') # Editar todos sus caracteristicas
-
-class ActualizarSocio(forms.ModelForm): # Kev
-    class Meta:
-        model = Socio
-        fields = ('__all__')
-
 
 class CrearNuevoAutor(forms.Form):
     nombre = forms.CharField(label='Nombre del autor', max_length=200)
@@ -27,3 +16,18 @@ class CrearNuevoSocio(forms.Form):
     nombre = forms.CharField(label='Nombre del autor', max_length=50)
     apellido = forms.CharField(label='Apellido del autor', max_length=50)
     fecha_nacimiento = forms.DateField(label='Fecha de nacimiento (YYYY-MM-DD)')
+
+class ActualizarAutor(forms.ModelForm): # Kev
+    class Meta:
+        model = Autor
+        fields = ('__all__') # Editar todos sus caracteristicas
+
+class ActualizarSocio(forms.ModelForm): # Kev
+    class Meta:
+        model = Socio
+        fields = ('__all__')
+
+class ActualizarLibro(forms.ModelForm): # Nai
+    class Meta:
+        model = Libro
+        fields = ('__all__')
