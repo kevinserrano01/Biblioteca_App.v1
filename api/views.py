367d613be5bb0d57d3bc5dd9dd2aa842a98b61d0
Naira <88989496+Naikl12@users.noreply.github.com>
2023-06-02 13:20:07 -0300

    view de listado de libros en api

--- api/views.py
+++ api/views.py
@@ -10,12 +10,26 @@ from django.db.models import F, Value
 from django.db.models.functions import Concat
 
 def detalle_libro(request, libro_id):
-    libro = get_object_or_404(Libro,id=libro_id)
-    libro_dict=model_to_dict(libro)
-    return JsonResponse(libro_dict, safe=False)
-
+    try:
+        libro = Libro.objects.get(id=libro_id)
+        libro_dict = {
+            'id': libro.id,
+            'titulo': libro.titulo,
+            'descripcion': libro.descripcion,
+            'autor': str(libro.autor)  
+        }
+        return JsonResponse(libro_dict, safe=False)
+    except Libro.DoesNotExist:
+        return JsonResponse([], safe=False)
+    
 def listado_libros(request):
-    libros = Libro.objects.annotate(autor_nombre_completo=Concat(F('autor_nombre'), Value(' '), F('autor_apellido')))
-    libros = libros.values('id', 'titulo', autor_=F('autor_nombre_completo'))
-    return JsonResponse(list(libros), safe=False)
-
+    libros = Libro.objects.all()
+    libros_dict = [
+        {
+            'id': libro.id,
+            'titulo': libro.titulo,
+            'autor': str(libro.autor) 
+        }
+        for libro in libros
+    ]
+    return JsonResponse(libros_dict,safe=False)
