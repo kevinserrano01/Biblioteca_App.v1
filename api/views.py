94e15e27d1abb591c426685a458ae329a6885e6f
Kev <kevinserrano010@gmail.com>
2023-06-02 12:43:15 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -6,27 +6,16 @@ from django.forms.models import model_to_dict
 from django.shortcuts import render
 from biblioteca.models import Libro
 from django.http import JsonResponse
-from django.db.models import F
-
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 
 def detalle_libro(request, libro_id):
     libro = get_object_or_404(Libro,id=libro_id)
     libro_dict=model_to_dict(libro)
     return JsonResponse(libro_dict, safe=False)
 
-
-
-#Nai
-# def listado_libros(request):
-#     libros = list(map(lambda libro: libro.pop('descripcion'), Libro.objects.values())) #lista de libros sin su descripcion
-#     return JsonResponse(libros, safe=False)
-
-
-
-
-
-####GUS### MUESTRA TODOS LOS LIBROS EN FORMATO JSON
 def listado_libros(request):
-    libros = list(Libro.objects.values('id', 'titulo', Autor= F('autor__nombre')))
-    return JsonResponse(libros,safe=False)
+    libros = Libro.objects.annotate(autor_nombre_completo=Concat(F('autor_nombre'), Value(' '), F('autor_apellido')))
+    libros = libros.values('id', 'titulo', autor_=F('autor_nombre_completo'))
+    return JsonResponse(list(libros), safe=False)
 
