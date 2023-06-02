173f5bfe05c678d03ba877c7a8cdac68fc3b5f28
Gustavo Ledesma <gustavo.lede@gmail.com>
2023-06-01 22:43:20 -0300

    varias modificaciones

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -22,11 +22,12 @@ urlpatterns = [
     path('prestamos/modificar/<int:prestamoLibro_id>', views.actualizar_Prestamo_Libro, name='actualizar_prestamo_libro'),
     path('prestamos/eliminar/<int:prestamoLibro_id>', views.eliminar_regPrestamo, name='eliminar_reg_prestamo_libro'),
     path('socios/actualizar/<int:socio_id>', views.actualizar_socios, name='actualizar_socio'),
-    path('libros/activar/<int:libro_id>', views.activar_registro_libro, name='activar_registro_libro'),
+    path('libros/activar/<int:libro_id>', views.activar_registro_libro, name='activar_libro'),
     path('prestamos/listado/', views.listado_prestamolibro, name='listado_prestamolibro'),
     path('prestamos/nuevo/', views.nuevo_prestamo_libro, name='nuevo_prestamo_libros'),
     path('prestamos/modificar/<int:prestamoLibro_id>', views.actualizar_Prestamo_Libro, name='actualizar_prestamo_libro'),
-     path('libro/listado/', views.listado_libro, name='listado_libro'),
+    path('libro/listado/', views.listado_libro, name='listado_libro'),
+    path('libros/desactivar/<int:libro_id>', views.desactivar_libro, name='desactivar_libro'),
 ]
 
 
